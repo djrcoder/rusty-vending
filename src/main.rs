c1a09a29af1af00f1f3d7f1e062bca38cc6912f4
djrcoder <djrcode@gmail.com>
2020-12-11 23:59:41 +0000

    Number guessing game.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guesss the number between 0 and 100!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        // println!("The secret number is {} ", secret_number);
+
+        println!("Input guess now: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Winner winner chicken dinner 🐔");
+                break;
+            }
+        }
+    }
 }
